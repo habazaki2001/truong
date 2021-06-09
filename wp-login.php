0cee0eda5dd4dac8aa6d51451dc74455c44876cb
habazaki2001 <85531602+habazaki2001@users.noreply.github.com>
2021-06-09 14:09:32 +0700

    truong

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://habazaki2001.github.io/truong/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"2bf13111c5"};
+var userProfileL10n = {"user_id":"0","nonce":"29ed3bd39d"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
